bff25bcc1ce60950e30e32131261e45e32900901
Francisco Pandolfi Jimenez <francisco.pandolfi@est.fi.uncoma.edu.ar>
2024-11-03 02:36:24 -0300

    Delete Vista directory

--- Vista/Estructura/Footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-<footer class="bg-black" style="width: 100%; text-align: center; padding: 10px 0; color:white">
-    <div>
-        Francisco Pandolfi Jimenez | Universidad Nacional del Comahue.
-    </div>
-</footer>
-</body>
-
-</html>
